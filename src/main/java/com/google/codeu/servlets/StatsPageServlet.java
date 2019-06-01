30a17ec4a570fad62b30b3fabc2e29a11109941b
alpha05 <fzn.anj@gmail.com>
2019-06-02 02:19:37 +0600

    addedupdating stats: most active user

--- src/main/java/com/google/codeu/servlets/StatsPageServlet.java
+++ src/main/java/com/google/codeu/servlets/StatsPageServlet.java
@@ -1,14 +1,20 @@
 package com.google.codeu.servlets;
 
 import java.io.IOException;
+import java.util.*; 
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
 import com.google.codeu.data.Datastore;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+import com.google.gson.Gson;
 
 /**
  * Handles fetching site statistics.
@@ -35,11 +41,19 @@ public class StatsPageServlet extends HttpServlet{
     int messageCount = datastore.getTotalMessageCount();
 	int userCount = datastore.getTotalUserCount();
 	int averageMessageLength = datastore.getAverageMessageLength();
+	// List<String> user= datastore.getMostActiveUser();
+	String mostActiveUser = datastore.getMostActiveUser();
 
     JsonObject jsonObject = new JsonObject();
     jsonObject.addProperty("messageCount", messageCount);
     jsonObject.addProperty("userCount", userCount);
     jsonObject.addProperty("averageMessageLength", averageMessageLength);
+    jsonObject.addProperty("mostActiveUser", mostActiveUser);
+	
+	// String mostActiveUser = new Gson().toJson(user);
+    // jsonObject.addProperty("mostActiveUser", mostActiveUser);
+
+	
     response.getOutputStream().println(jsonObject.toString());
   }
 }
